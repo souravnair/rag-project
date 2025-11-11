a7b81e940caa21be832bd8645fa3b4818dfe0685
sourav nair <souravnair2k@gmail.com>
2025-11-11 16:29:40 +0530

    added token text splitter

--- dataingestion/dataingestion.py
+++ dataingestion/dataingestion.py
@@ -1,8 +1,9 @@
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import TextSplitter,RecursiveCharacterTextSplitter,CharacterTextSplitter
+from langchain.text_splitter import TextSplitter,RecursiveCharacterTextSplitter,CharacterTextSplitter,TokenTextSplitter
 loader=TextLoader('/home/sourav/rag-course-krishnaik/documents/sample.txt',encoding='utf-8')
 documents=loader.load()
 print(documents)
+#character text splitter
 splitter=CharacterTextSplitter(
     separator="\n",
     chunk_size=10,
@@ -11,6 +12,8 @@ splitter=CharacterTextSplitter(
 )
 content_chunks=splitter.split_text(documents[0].page_content)
 print(content_chunks)
+
+#recursive character text splitter
 recursive_splitter=RecursiveCharacterTextSplitter(
     separators=["\n"," ",""],
     chunk_size=10,
@@ -19,3 +22,12 @@ recursive_splitter=RecursiveCharacterTextSplitter(
 )
 content_chunks_recursive=recursive_splitter.split_text(documents[0].page_content)
 print(content_chunks_recursive)
+
+#token text splitter
+token_splitter=TokenTextSplitter(
+    chunk_size=10, 
+    chunk_overlap=2
+    )
+
+content_chunks_tokens=token_splitter.split_text(documents[0].page_content)
+print(content_chunks_tokens)
